81ba4bb5a9d9a783a3caefc34a6e22163bcf09e9
Shruti Priya <shrutipriya1808@gmail.com>
2017-05-05 10:24:18 -0700

    add comments to the StartUp.cs file

--- src/MrFixIt/Startup.cs
+++ src/MrFixIt/Startup.cs
@@ -27,19 +27,26 @@ namespace MrFixIt
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+
+            //This method configures the required framework services for our application
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(); // adds the MVC service to the project.
 
             services.AddEntityFramework()
                 .AddDbContext<MrFixItContext>(options =>
                     options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+
+
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<MrFixItContext>()
                 .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+            // Configure method = Tells our app what frameworks to use.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             
@@ -52,11 +59,14 @@ namespace MrFixIt
 
             app.UseIdentity();
 
+            // app.UseMvc() tells our app that it will be using the MVC framework.
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Account}/{action=Index}/{id?}");
+
+                //tells our app to use Index action of Account Controller to ensure user authentication.
             });
 
             // add some configuration to our app to tell it to use static files. Also, app.UseStaticFiles() needs to be before app.Run..., or the files will not load.
